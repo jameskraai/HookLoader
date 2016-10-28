41e038ec72c5dfca05245d83c5115fb216bcff6b
James Kraai <james@jameskraai.com>
2016-10-27 22:12:00 -0500

    Add a TestCase class that our tests can inherit from. Load our tests directory using psr 4

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+/**
+ * Base test case for the rest of our library tests.
+ */
+class TestCase extends PHPUnit
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        Mockery::close();
+    }
+}
